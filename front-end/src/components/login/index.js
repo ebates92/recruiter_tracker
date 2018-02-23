0efcc636d4074d56f89e36f59f338d07c0c30e22
Evan Bates <ebates92@gmail.com>
2018-02-23 17:48:11 -0500

    form is functional and recording props

--- front-end/src/components/login/index.js
+++ front-end/src/components/login/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import Authentication from '../../authentication'
-import { Redirect } from 'react-router-dom'
+import { Redirect, Link } from 'react-router-dom'
 
 // this will be the login page component
 class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            redirectToReferrer: false
+            redirectToReferrer: false,
+            firstname: '',
+            lastname: '',
+            email: '',
+            password: '',
         }
+        // this.onLoginFormChangeHandler = this.onLoginFormChangeHandler.bind(this);
     }
 
     // within a class you don't need to use const or let
+    // will take you to the authentication login for what you submit
     login = () => {
         Authentication.authenticate(() => {
             this.setState({ redirectToReferrer: true })
         })
+    }
+
+    onLoginFormChangeHandler = (event) => {
+        const input = event.target.name;
+        const value = event.target.value;
+        this.setState({
+              [input]: value
+            }
+        )
     }
 
     
@@ -30,9 +45,37 @@ class Login extends React.Component {
         }
 
         return (
-            <div>
-                <button onClick={this.login}>Log in</button>
-                <h1>I'M WORKING</h1>
+            <div className="login-container">
+
+                <div className="login-title">Sign-in with:</div>
+                <div className="login-passport">
+                    <button onClick={this.login}>LinkedIn</button>
+                    <button onClick={this.login}>Google</button>
+                </div>
+
+                <div className="login-email-password">
+                    <input type='email' name='email' onChange={this.onLoginFormChangeHandler} placeholder='email' />
+                    <input type='password' name='password' onChange={this.onLoginFormChangeHandler} placeholder='password' />
+                    <button className="sign-in" onClick={this.login}>Sign in</button>
+                    <Link className="forgot-password" to='/forgotpassword'>Forgot password</Link>
+                </div>
+
+                <div className="login-email-password">
+
+                    <label>First name:</label>
+                    <input name="firstname" type='text' value={this.state.firstname} onChange={this.onLoginFormChangeHandler} />
+
+                    <label>Last name:</label>
+                    <input name="lastname" type='text' onChange={this.onLoginFormChangeHandler} />
+
+                    <label>Email:</label>
+                    <input name="email" type='email' onChange={this.onLoginFormChangeHandler} />
+
+                    <label>Password:</label>
+                    <input name="password" type='password' onChange={this.onLoginFormChangeHandler} />
+
+                    <button onClick={this.login}>Join for free</button>
+                </div>
             </div>
         )
     }
