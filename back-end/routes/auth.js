e572eee102c0cccb367fe34b9100b12521f091a8
Tyler Campbell <tcampb30@gmail.com>
2018-02-24 13:29:35 -0500

    Initializing auth branch

---
+++ back-end/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const Posting = require('../models/table/posting');
+const Applicant = require('../models/table/applicant');
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
