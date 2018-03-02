dc892f60aca88c8185717914f9e41ab9d431a5ee
Evan Bates <ebates92@gmail.com>
2018-03-02 11:39:02 -0500

    searchbar is fully functional

--- front-end/src/components/dashboard/header/SearchBar.js
+++ front-end/src/components/dashboard/header/SearchBar.js
@@ -24,19 +24,37 @@ class SearchBar extends Component {
         })
     }
 
-    _onFocusForDropdownsPosting = (event) => {
+    _onFocusForDropdowns = (event) => {
         this.setState({
             dropdownMainSearch: DropdownMainSearchComponent,
             postingOptions: this.props.postingRecords,
             applicantOptions: this.props.applicantRecords,
         })
-        document.querySelector('.searchbar-wrapper').addEventListener('mouseleave', (event) => {
+        document.querySelector('.whole-searchbar').addEventListener('mouseleave', (event) => {
             this.setState({
                 dropdownMainSearch: null,
                 mainSearch: ''
             })
+            document.querySelector('.whole-searchbar').addEventListener('mouseenter', (event) => {
+                this.setState({
+                    dropdownMainSearch: DropdownMainSearchComponent,
+                    postingOptions: this.props.postingRecords,
+                    applicantOptions: this.props.applicantRecords,
+                })
+            })
         })
     }
+
+    _onBlurForDropdowns = (event) => {
+        document.querySelector('.whole-searchbar').removeEventListener('mouseenter', (event) => {
+            this.setState({
+                dropdownMainSearch: DropdownMainSearchComponent,
+                postingOptions: this.props.postingRecords,
+                applicantOptions: this.props.applicantRecords,
+            })
+        })
+    }
+
 
     _onChangeForDropdowns = (event) => {
         
@@ -49,22 +67,34 @@ class SearchBar extends Component {
                   let postingOptions = this.props.postingRecords.filter((posting) => posting.positionTitle.match(expression))
                   this.setState({
                         applicantOptions,
+                        postingOptions,
                         searchTerm: event.target.value
                     })
                 }
+
+    _onClickSearchDropdown = (event) => {
+        this.setState({
+            searchTerm: event.target.innerHTML,
+            dropdownMainSearch: null
+        })
+    }
 
     render() {
         const DropdownMainSearch = this.state.dropdownMainSearch || Nothing;
         const listItemInfoStyle = {margin: '0px', paddingBottom: '3px'};
         const iconStyle = {fontSize: '.8125rem', color: '#b0adab'};
         const divStyle = {paddingBottom: '3px'};
-        return <div className="searchbar-wrapper">
-                    <div className="searchbar">
-                        <input type="text" placeholder="Search for Posting or Applicant..."/>
+        return (
+                <div className='whole-searchbar'>
+                    <div className="searchbar-wrapper">
+                        <div className="searchbar">
+                            <input type="text" value={this.state.searchTerm} placeholder="Search for Posting or Applicant..." onChange={this._onChangeForDropdowns} onFocus={this._onFocusForDropdowns} onBlur={this._onBlurForDropdowns}/>
+                        </div>
+                        <div className='search-icon' style={divStyle}><i className="fas fa-search" style={iconStyle}></i></div> 
                     </div>
-                    <div style={divStyle}><i className="fas fa-search" style={iconStyle}></i></div>
-                    <DropdownMainSearch applicantOptions={this.state.applicantOptions} postingOptions={this.state.postingOptions}/>
+                    <DropdownMainSearch onClickSearchDropdown={this._onClickSearchDropdown} applicantOptions={this.state.applicantOptions} postingOptions={this.state.postingOptions}/>
                 </div>
+        )
     }
 
 }
