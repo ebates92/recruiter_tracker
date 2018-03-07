838e26787aef53f20400a094556a9c801225efb3
Evan Bates <ebates92@gmail.com>
2018-03-07 15:53:27 -0500

    edited and fixed header data

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -51,6 +51,12 @@ router.get('/google', passport.authenticate('google', {
    })
   })
 
+  // Handle Logout
 
+  .post('/logout', (req,res) => {
+    // res.send('logging out')
+    res.clearCookie('session')
+    res.send('http://localhost:3000/login')
+  })
 
 module.exports = router;
