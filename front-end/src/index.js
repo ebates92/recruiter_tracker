9dc730eb1b6611eb6c435aa1332f2bde85e3b321
Evan Bates <ebates92@gmail.com>
2018-03-30 12:10:11 -0700

    adding in ajax calls into redux store

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
