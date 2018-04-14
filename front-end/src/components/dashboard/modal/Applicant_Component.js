e59b7e1d7010f167b23ed221e3c8a4b3a999e94e
Evan Bates <ebates92@gmail.com>
2018-04-14 16:15:11 +0900

    finally does with redux

--- front-end/src/components/dashboard/modal/Applicant_Component.js
+++ front-end/src/components/dashboard/modal/Applicant_Component.js
@@ -3,32 +3,45 @@ import PostingsApplied from './Postings_applied'
 // import ModalTypes from './Modal-types.js';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import updateSelectedApplicant from '../../../actions/selected.js'
 
  class ApplicantComponent extends Component {
 
+    closeModal = () => {
+        this.props.updateSelectedApplicant({
+            target: {id:'applicant'},
+            currentTarget: {accessKey: null}
+          })
+    }
+
+
     render() {
-        const currentApplicant = this.props.applicantData
+        const currentApplicant = this.props.applicantData.filter((data) => {return data.id === parseInt(this.props.selectedApplicant)})
+
+        const currentApplicantsPostings = this.props.postingApplicantData.filter((postingApplicant) => {
+            return (postingApplicant.applicantId === parseInt(this.props.selectedApplicant))
+          })
 
         return (
             <div className="modal-overlay" data-modal-container-applicantcomponent>
                 <div className="modal-container">
-                    <div className="modal-header"><h2><strong>{this.props.applicantData[0].firstName} {this.props.applicantData[0].lastName}</strong></h2></div>
+                    <div className="modal-header"><h2><strong>{currentApplicant[0].firstName} {currentApplicant[0].lastName}</strong></h2></div>
                     <div className="modal-body applicant-component-body">
                         <div className='applicant-component-left-side'>
                             <div className='applicant-component-section-title'>Personal Information</div>
                             <div className='applicant-component-information-container'>
                                 <div className='applicant-component-information'>
-                                    <div className="applicant-component-datafield"><strong>{this.props.applicantData[0].firstName} {this.props.applicantData[0].lastName}</strong></div>
-                                    <div className="applicant-component-datafield">{this.props.applicantData[0].email}</div>
-                                    <div className="applicant-component-datafield">{this.props.applicantData[0].phone}</div>
-                                    <div className="applicant-component-datafield"><a href='{this.props.applicantData[0].linked_in}'>{this.props.applicantData[0].linked_in}</a></div>
-                                    <div className="applicant-component-datafield">{this.props.applicantData[0].recruiter_notes}</div>
+                                    <div className="applicant-component-datafield"><strong>{currentApplicant[0].firstName} {currentApplicant[0].lastName}</strong></div>
+                                    <div className="applicant-component-datafield">{currentApplicant[0].email}</div>
+                                    <div className="applicant-component-datafield">{currentApplicant[0].phone}</div>
+                                    <div className="applicant-component-datafield"><a href='{currentApplicant[0].linked_in}'>{currentApplicant[0].linked_in}</a></div>
+                                    <div className="applicant-component-datafield">{currentApplicant[0].recruiter_notes}</div>
                                 </div>
                             </div>
-                            {/* <div>
+                            <div>
                                 <div className='applicant-component-section-title'>Relevant Postings</div>
-                                <PostingsApplied postingRecords={this.props.postingData} currentApplicantsPostings={this.props.currentApplicantsPostings}/>
-                            </div> */}
+                                <PostingsApplied postingRecords={this.props.postingData} currentApplicantsPostings={currentApplicantsPostings}/>
+                            </div>
                         </div>
                         <div className='applicant-component-right-side'>
                             <div></div>
@@ -37,7 +50,7 @@ import { bindActionCreators } from 'redux';
                     
                     </div>
                     <div className="modal-footer">
-                        <button id='applicantcomponent' onClick={this.props.closeModalCorrectly}>Cancel</button>
+                        <button id='applicantcomponent' onClick={this.closeModal}>Cancel</button>
                         {/* <button>Save & New</button> */}
                         {/* {<button type="submit" id='posting' onClick={this.props.onFormSubmission}>Save</button>} */}
                     </div>
@@ -48,17 +61,20 @@ import { bindActionCreators } from 'redux';
 }
 
 // REDUX APPLICATION STATE (COMBINED REDUCERS)
-function mapStateToProps({ postingData, applicantData, selectedApplicant }) {
+function mapStateToProps({ postingData, applicantData, selectedApplicant, postingApplicantData }) {
     return {
         postingData,
         applicantData,
-        selectedApplicant
+        selectedApplicant,
+        postingApplicantData
     }
 }
 
 // REDUX EVENT HANDLERS (ACTIONS)
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({},dispatch)
+    return {
+        updateSelectedApplicant: (event) => dispatch(updateSelectedApplicant(event))
+    }
 }
 
 export default connect(mapStateToProps,mapDispatchToProps)(ApplicantComponent);
